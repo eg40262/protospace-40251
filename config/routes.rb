d9c14ad29a4dac56da3e4d996658ec67b12f5c7f
eg40262 <eg40262@gmail.com>
2024-03-25 15:12:24 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:'prototypes#index'
-  resources :prototypes
-  resources :users, only: :show
-  resources :comments, only: :create
-  
+  root to: 'prototypes#index'
+  resources :users, only: :show  
   resources :prototypes do
     resources :comments, only: [:create]
   end
